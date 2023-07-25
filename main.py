be278a397eb7f7b62fcd2464d5ce4d2fb53689c5
kourosh <kmh.mahmoudi@gmail.com>
2023-07-25 17:21:12 +0330

    error pages

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+#Home page
 @app.route('/')
 def index():
     return render_template("index.html")
 
+# User page
 @app.route('/user/<name>')
 def user(name):
-    return f"<h1>Hello {name}</h1>"
+    return render_template("user.html", user_name=name)
+
+#Custome Error Page
+#Invalid URL error
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Internal server error error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
