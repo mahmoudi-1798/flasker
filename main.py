e6266a5e8fac2f57323735b3d27ffe24cd60e9cb
kourosh <kmh.mahmoudi@gmail.com>
2023-07-26 20:34:42 +0330

    adding name form

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+
+#load the environmental variables from the .env file
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my super secret key"
+
+#Form class, inherited from FlaskForm
+class NamerForm(FlaskForm):
+    name = StringField("Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 #Home page
 @app.route('/')
@@ -22,3 +36,14 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     return render_template("500.html"), 500
+
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form 
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html", name=name, form=form)
