ebcc4fee0a56516a770cf48ca20eb53e386c4ffd
kourosh <kmh.mahmoudi@gmail.com>
2023-07-29 18:12:12 +0330

    adding sqlite db

--- main.py
+++ main.py
@@ -3,18 +3,37 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #load the environmental variables from the .env file
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SECRET_KEY"] = "my super secret key"
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+class UserForm(FlaskForm):
+    name = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #Form class, inherited from FlaskForm
 class NamerForm(FlaskForm):
     name = StringField("Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
 
 #Home page
 @app.route('/')
@@ -48,3 +67,24 @@ def name():
         form.name.data = ''
         flash("Form Submitted Successfully.") #To show a message at top after submiting and entering
     return render_template("name.html", name=name, form=form)
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("Form Submitted Successfully.") #To show a message at top after submiting and entering
+        else:
+            flash("User with this email exsist.") 
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
+
+    
